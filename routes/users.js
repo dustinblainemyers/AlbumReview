767b5c41520aab73bb1fef488510550a518cf6d7
Ally Brannon <allison.brannon@gmail.com>
2020-03-05 09:59:59 -0500

    add login and signup

--- routes/users.js
+++ routes/users.js
@@ -1,11 +1,27 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-  
+router.get("/signup", async (req, res, next) => {
+  res.render("template", {
+    locals: {
+      title: "signup"
+    },
+    partials: {
+      partial: "partial-signup"
+    }
+  });
+});
 
+router.get("/login", async (req, res, next) => {
+  res.render("template", {
+    locals: {
+      title: "login"
+    },
+    partials: {
+      partial: "partial-login"
+    }
+  });
 });
 
 module.exports = router;
